c5392edb7828aef52cac0bfa22f4c2c7c6518f69
Hasibul-Islam-Shanto <mdhasibulislamshanto007@gmail.com>
2025-01-21 23:48:31 +0600

    feat: add new project on project section

--- constants/projects.ts
+++ constants/projects.ts
@@ -1,6 +1,20 @@
+import { describe } from "node:test";
+
 export const projects = [
   {
     id: 1,
+    title: "Hotel Booking App",
+    description: [
+      "Users can book rooms, make payments, and leave reviews, while hotel owners can create, update, and manage their listings.",
+      "Secure payment processing with automatic email receipts and downloadable options for users.",
+      "Role-based access control and secure authentication to ensure safe and personalized user experiences.",
+    ],
+    img: "/projects/hotel-booking.png",
+    iconLists: ["/react.svg", "/next.svg", "/ts.svg", "/mongodb.svg"],
+    link: "https://github.com/Hasibul-Islam-Shanto/hotel-booking",
+  },
+  {
+    id: 2,
     title: "E-commerce",
     description: [
       "It’s a full-stack E-Commerce web application.",
@@ -18,7 +32,7 @@ export const projects = [
     link: "https://github.com/smalhasib/ecommerce",
   },
   {
-    id: 2,
+    id: 3,
     title: "Ticketing System with Web3",
     description: [
       "Developed a decentralized ticketing system using React.js, Ether.js, Solidity, and IPFS database, enabling secure and transparent transactions.",
@@ -28,18 +42,6 @@ export const projects = [
     img: "/projects/online-ticketing.png",
     iconLists: ["/react.svg", "/sass.svg", "/js.svg", "/solidity.svg"],
     link: "https://github.com/Hasibul-Islam-Shanto/project350",
-  },
-  {
-    id: 3,
-    title: "Share",
-    description: [
-      "Created a social app, Share, with Next.js, Firebase, Tailwind CSS, and Google OAuth for user authentication.",
-      "Enabled features for creating, deleting, liking, and commenting on posts.",
-      "Used Firebase for real-time data handling and secure user interactions.",
-    ],
-    img: "/projects/share.png",
-    iconLists: ["/react.svg", "/next.svg", "/ts.svg", "/firebase.svg"],
-    link: "https://github.com/Hasibul-Islam-Shanto/share",
   },
   {
     id: 4,
